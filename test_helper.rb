c5312cd7bba885be6f985b2e5502c8a438780f4a
Adam Price <komidore64@gmail.com>
2020-05-29 14:15:30 -0400

    initial file structure

---
+++ test_helper.rb
@@ -0,0 +1,22 @@
+# code-challenge
+# Copyright (C) 2020  M. Adam Price
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'minitest/autorun'
+
+Rake::FileList.new(File.join(File.dirname(__FILE__), "test", "*_test.rb")).each do |f|
+  require f
+end
