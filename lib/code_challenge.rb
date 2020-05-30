c4eb1c43bd64abfbefa37917ad9b925a8268ff84
Adam Price <komidore64@gmail.com>
2020-05-30 15:38:11 -0400

    quick repl script for ease of talking with objects

--- lib/code_challenge.rb
+++ lib/code_challenge.rb
@@ -17,13 +17,5 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-source 'https://rubygems.org'
-
-ruby '~> 2.7.0'
-
-group :development do
-  gem 'minitest', '~> 5.14.1'
-  gem 'pry', '~> 0.13.1'
-  gem 'rake', '~> 13.0.1'
-  gem 'rubocop', '~> 0.84.0'
-end
+require 'code_challenge/card'
+require 'code_challenge/deck'
